41f5b734e6421ec5f1156115383752706742e8fe
catalina-delgado <catalina08delgado@gmail.com>
2025-01-12 22:51:46 -0500

    edit imports

--- imports.py
+++ imports.py
@@ -8,12 +8,16 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import optimizers, regularizers, backend as K
 
 # Adding the imports to the global namespace, this file is personalizable
-from sklearn.model_selection import train_test_split
+
+from keras.initializers import HeNormal
+from keras.optimizers import SGD
+# from sklearn.model_selection import train_test_split
+
 
 import json
-import pickle
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from transformers import BertTokenizer, BertModel
-import torch
+# import pickle
+# import nltk
+# from nltk.stem import WordNetLemmatizer
+# from nltk.corpus import stopwords
+# from transformers import BertTokenizer, BertModel
+# import torch
