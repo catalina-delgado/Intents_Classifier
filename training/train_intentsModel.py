d2e641d287f504e15edb9b662b8447afb628a91a
catalina-delgado <catalina08delgado@gmail.com>
2025-01-12 22:46:42 -0500

    update trainig stage

--- training/train_intentsModel.py
+++ training/train_intentsModel.py
@@ -1,5 +1,6 @@
 from functions import NeuroStage
 from imports import tf, np
+import os
 from src.models.model import Model
 
 class TrainModel(NeuroStage):
@@ -10,10 +11,11 @@ class TrainModel(NeuroStage):
         self.MODEL_NAME = model_name
        
     def train(self):
-        X_train = np.random.rand(100, 256, 256, 1)
-        y_train = np.random.randint(0, 2, 100) 
-        X_val = np.random.rand(20, 256, 256, 1) 
-        y_val = np.random.randint(0, 2, 20)
+        print(os.getcwd())
+        X_train = np.load('data/X_train.npy')
+        y_train = np.load('data/y_train.npy')
+        X_val = np.load('data/X_val.npy')
+        y_val = np.load('data/y_val.npy')
         
         self.architecture = Model(X_train, y_train)
         self.model = self.architecture.compile()
